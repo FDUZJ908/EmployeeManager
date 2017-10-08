379eec3d6e81285a178c7b4125c46d200e09d5f0
lsh <lshzy137@gmail.com>
2017-10-08 21:52:31 +0800

    mapper bug fixed

--- src/main/java/EmployeeManager/Mapper.java
+++ src/main/java/EmployeeManager/Mapper.java
@@ -6,10 +6,9 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.List;
 import java.util.ArrayList;
-import java.util.Map;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by lsh on 08/10/2017.
@@ -17,35 +16,35 @@ import java.util.HashMap;
 public class Mapper<T> implements RowMapper<T> {
 
     private Class<T> cls;
-    private Map<String,Object> defValue;
+    private Map<String, Object> defValue;
 
     Mapper(Class<T> _cls) {
         cls = _cls;
     }
 
-    Mapper(Class<T> _cls, Map<String,Object> _defValue){
+    Mapper(Class<T> _cls, Map<String, Object> _defValue) {
         cls = _cls;
-        Map<String,Object> defValue=_defValue;
+        Map<String, Object> defValue = _defValue;
     }
 
     public T mapRow(ResultSet result, int rowNum) throws SQLException {
         try {
             T obj = cls.newInstance();
-            Field field=cls.getDeclaredField("Attrs");
-            String[] Attrs=(String[])(field.get(obj));
-            List<Object> argv=new ArrayList<Object>();
-            for(String attr:Attrs){
+            Field field = cls.getDeclaredField("Attrs");
+            String[] Attrs = (String[]) (field.get(obj));
+            Map<String, Object> argv = new HashMap<String, Object>();
+            for (String attr : Attrs) {
                 try {
-                    argv.add(result.getObject(attr));
-                }catch (SQLException e) {
-                    if(defValue.containsKey(attr)) argv.add(defValue.get(attr));
-                    else argv.add("");
+                    argv.put(attr, result.getObject(attr));
+                } catch (SQLException e) {
+                    if (defValue.containsKey(attr)) argv.put(attr, defValue.get(attr));
+                    else argv.put(attr,"");
                 }
             }
-            Constructor cons=cls.getDeclaredConstructor(ArrayList.class);
-            return (T)(cons.newInstance(argv));
+            Constructor cons = cls.getDeclaredConstructor(Map.class);
+            return (T) (cons.newInstance(argv));
         } catch (Exception e) {
-            System.out.println("error: "+e.getMessage());
+            System.out.println("error: " + e.getMessage());
             return null;
         }
     }
