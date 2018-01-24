a582bc7db4262d04713d8d3f4ecc70a8c163c6b8
Joash <1143768516@qq.com>
2018-01-25 00:52:18 +0800

    privilege By Joash

--- src/main/java/EmployeeManager/admin/controller/PrivilegeController.java
+++ src/main/java/EmployeeManager/admin/controller/PrivilegeController.java
@@ -3,6 +3,8 @@ package EmployeeManager.admin.controller;
 import EmployeeManager.admin.application.PrivilegeService;
 import EmployeeManager.admin.model.Privilege;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("/privilege")
 public class PrivilegeController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     protected PrivilegeService privilegeService;
 
@@ -24,6 +28,40 @@ public class PrivilegeController {
         return "privilege/list";
     }
 
+    @RequestMapping(value = "/add",method = RequestMethod.GET)
+    public String add(){
+        return "privilege/formAdd";
+    }
+
+    @RequestMapping(value = "/modifyAdd",method = RequestMethod.POST)
+    public String modifyAdd(Privilege privilege){
+        privilegeService.create(privilege);
+        return "redirect:/privilege";
+    }
+
+    @RequestMapping(value = "/edit",method = RequestMethod.GET)
+    public String edit(@RequestParam("pid")String pid,Model model){
+        model.addAttribute("pid",pid);
+        model.addAttribute("privilege",privilegeService.get(pid));
+        return "privilege/formEdit";
+
+    }
+
+    @RequestMapping(value = "/modifyEdit",method = RequestMethod.POST)
+    public String modifyEdit(@RequestParam("pid")String pid,
+                             Privilege privilege){
+        privilege.setPid(pid);
+        privilegeService.modify(privilege);
+        return "redirect:/privilege";
+    }
+
+    @RequestMapping(value = "/delete", method = RequestMethod.DELETE)
+    @ResponseBody
+    public void delete(@RequestParam("pid") String pid) {
+        privilegeService.delete(pid);
+    }
+
+    /*
     @RequestMapping(value = "/form",method = RequestMethod.GET)
     public String toform(@RequestParam(value = "pid",required = false)String pid, Model model){
         String api="/privilege/add";
@@ -38,7 +76,7 @@ public class PrivilegeController {
     @RequestMapping(value = "/{pid}/modify",method = RequestMethod.POST)
     public String modify(@PathVariable("pid")String pid, Privilege privilege){
         privilege.setPid(pid);
-            privilegeService.modify(privilege);
+        privilegeService.modify(privilege);
         return "redirect:/privilege";
     }
 
@@ -53,5 +91,6 @@ public class PrivilegeController {
         privilegeService.create(privilege);
         return "redirect:/privilege";
     }
+    */
 
 }
