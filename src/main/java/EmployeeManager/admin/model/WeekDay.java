84118f0f1c19a37f3bc90e394be12927cd0c6cf3
lsh <lshzy137@gmail.com>
2018-03-03 16:44:19 +0800

    weekdays fixed

--- src/main/java/EmployeeManager/admin/model/WeekDay.java
+++ src/main/java/EmployeeManager/admin/model/WeekDay.java
@@ -1,47 +1,27 @@
 package EmployeeManager.admin.model;
 
+import EmployeeManager.Variable;
+
 /**
  * Created by 11437 on 2018/2/13.
  */
 public class WeekDay {
-    private String id;
+    private int id;
     private String name;
 
     public WeekDay() {
     }
 
-    public WeekDay(String id) {
+    public WeekDay(int id) {
         this.id = id;
-        switch (id) {
-            case "1":
-                this.name = "星期一";
-                break;
-            case "2":
-                this.name = "星期二";
-                break;
-            case "3":
-                this.name = "星期三";
-                break;
-            case "4":
-                this.name = "星期四";
-                break;
-            case "5":
-                this.name = "星期五";
-                break;
-            case "6":
-                this.name = "星期六";
-                break;
-            case "7":
-                this.name = "星期天";
-                break;
-        }
+        this.name = Variable.Weekdays[id];
     }
 
-    public String getId() {
+    public int getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(int id) {
         this.id = id;
     }
 
